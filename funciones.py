adc6b6d087cc1102ad9b3e14476b440c1326d560
Eduardo Zegers <eduardo.zgs@gmail.com>
2016-06-12 00:23:44 -0400

    obtener_canciones() implementado
    - Además se implementó filtro_palabras_largas()

--- funciones.py
+++ funciones.py
@@ -6,17 +6,40 @@
 # -*- coding: utf-8 -*-
 from random import *
 
-archivoCanciones="lista_de_canciones.txt"
-archivoPlaylist="listas_de_reproducciones.txt"
+archivoCanciones = 'lista_de_canciones.txt'
+archivoPlaylist  = 'listas_de_reproducciones.txt'
 
 def obtener_canciones(archivo):
-	return []
+	#Leer el archivo
+	fp 			= open(archivo, 'r')
+	canciones 	= []
+	for linea in fp:
+		canciones.append(linea.strip().split('<SEP>'))
+	#Ordenarlas por nombre de artista
+	ordenadas 	= []
+	for cancion in canciones:
+		ordenadas.append([cancion[2], cancion[0], cancion[1], cancion[3]])
+	ordenadas.sort()
+	#Filtrar las palabras
+	ordenadas 	= filtro_palabras_largas(ordenadas, 40)
+	#Unirlas en string nuevamente
+	canciones 	= []
+	for cancion in ordenadas:
+		canciones.append('<SEP>'.join([cancion[1], cancion[2], cancion[0], cancion[3]]))
+	#Cerrar el archivo
+	fp.close()
+	#Retornar la lista ordenada y filtrada
+	return canciones
 
 def filtro_busqueda(palabrasClaves,todasCanciones):
 	return []
 
 def filtro_palabras_largas(todasCanciones,maximo):
-	return []
+	filtradas = []
+	for cancion in todasCanciones:
+		if len(cancion[0]) < 40 and len(cancion[-1]) < 40:
+			filtradas.append(cancion)
+	return filtradas
 
 def generar_codigo(inicio,intermedio):
 	return ""
